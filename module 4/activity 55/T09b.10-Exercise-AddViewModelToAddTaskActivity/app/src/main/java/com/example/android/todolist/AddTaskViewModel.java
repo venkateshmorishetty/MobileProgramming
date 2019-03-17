54b69fb697e4159d14a05f2fb98507561561d7e5
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2019-03-17 23:01:28 +0530

    activity 55

---
+++ module 4/activity 55/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModel.java
@@ -0,0 +1,25 @@
+package com.example.android.todolist;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
+
+
+public class AddTaskViewModel extends ViewModel {
+
+
+    private LiveData<TaskEntry> task;
+
+
+    // Note: The constructor should receive the database and the taskId
+    public AddTaskViewModel(AppDatabase database, int taskId) {
+        task = database.taskDao().loadTaskById(taskId);
+    }
+
+
+    public LiveData<TaskEntry> getTask() {
+        return task;
+    }
+}
